0e01e74386515d4ea94d63f6c6fda6b308aeddc5
Xatalos <teemujsalminen@gmail.com>
2016-03-17 00:10:30 +0200

    Delete replayserver.js

--- src/main/webapp/js/replayserver.js
+++
@@ -1,9 +0,0 @@
-function showCreateRiddle() {
-    $("#riddles").addClass("hidden");
-    $("#createRiddle").removeClass("hidden");
-}
-
-function showRiddles() {
-    $("#createRiddle").addClass("hidden");
-    $("#riddles").removeClass("hidden");
-}
