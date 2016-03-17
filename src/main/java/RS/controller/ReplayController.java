fbb659552859d410cf4394566f078af0deac9827
Teemu Salminen <teemujsalminen@gmail.com>
2016-03-17 19:27:45 +0200

    removed some unnecessary files and added work-in-progress search functionality

--- src/main/java/RS/controller/ReplayController.java
+++ src/main/java/RS/controller/ReplayController.java
@@ -4,12 +4,12 @@ import RS.domain.Player;
 import RS.domain.Replay;
 import RS.repository.PlayerRepository;
 import RS.repository.ReplayRepository;
+import RS.util.SearchResult;
 import arkhados.replay.ReplayHeader;
 import arkhados.replay.ReplayMetadataSerializer;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import javax.transaction.Transactional;
-import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,71 +17,82 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-@Controller
-@RequestMapping("*")
+@RestController
 public class ReplayController {
 
     @Autowired
     private ReplayRepository replayRepository;
-
     @Autowired
     private PlayerRepository playerRepository;
 
-    //@RequestMapping(method = RequestMethod.GET)
-    public String listReplays(Model model) {
+    @CrossOrigin // allows AJAX calls from other websites
+    @RequestMapping(value = "/replays", method = RequestMethod.GET)
+    public SearchResult getReplays() {
         Pageable pageable = new PageRequest(0, 30, Sort.Direction.DESC, "gameDate");
         Page<Replay> replayPage = replayRepository.findAll(pageable);
-        model.addAttribute("replays", replayPage.getContent());
-        return "replays";
+        return new SearchResult(replayPage.getContent());
     }
 
+    @CrossOrigin
     @Transactional
-    @RequestMapping(value = "/topreplaystl", method = RequestMethod.GET)
-    public String topReplays(Model model, @RequestParam String sort) {
+    @RequestMapping(value = "/topreplays", method = RequestMethod.GET)
+    public SearchResult topReplays(@RequestParam String sort) {
         Pageable pageable = new PageRequest(0, 30, Sort.Direction.DESC, "gameDate");
         if (sort.equals("downloads")) {
             pageable = new PageRequest(0, 30, Sort.Direction.DESC, "downloads");
         }
         Page<Replay> replayPage = replayRepository.findAll(pageable);
-        model.addAttribute("replays", replayPage.getContent());
-        return "replays";
+        return new SearchResult(replayPage.getContent());
     }
 
-    @RequestMapping(value = "/newreplaytl", method = RequestMethod.POST)
-    public String addReplay(@RequestParam("replay") MultipartFile file, @Valid @ModelAttribute Replay replay,
-            BindingResult result, RedirectAttributes redirectAttributes) throws IOException {
+    @CrossOrigin
+    @Transactional
+    @RequestMapping(value = "/searchreplays", method = RequestMethod.GET)
+    public SearchResult searchReplays(@RequestParam String version,
+            @RequestParam String arena, @RequestParam String mode) {
+        // TODO: search by player... and downloads?
+        Pageable pageable = new PageRequest(0, 30, Sort.Direction.DESC, "gameDate");
+        Page<Replay> replayPage = replayRepository.findAll(pageable);
+        if (!version.equals("any") && mode.equals("any") && arena.equals("any")) {
+            replayPage = replayRepository.findByVersion(pageable, version);
+        } else if (version.equals("any") && !mode.equals("any") && arena.equals("any")) {
+            replayPage = replayRepository.findByGameMode(pageable, version);
+        } else if (version.equals("any") && mode.equals("any") && !arena.equals("any")) {
+            replayPage = replayRepository.findByArena(pageable, version);
+        } else if (!version.equals("any") && !mode.equals("any") && arena.equals("any")) {
+            replayPage = replayRepository.findByVersionAndGameMode(pageable, version, mode);
+        } else if (!version.equals("any") && mode.equals("any") && !arena.equals("any")) {
+            replayPage = replayRepository.findByVersionAndArena(pageable, version, arena);
+        } else if (version.equals("any") && !mode.equals("any") && !arena.equals("any")) {
+            replayPage = replayRepository.findByGameModeAndArena(pageable, mode, arena);
+        } else if (!version.equals("any") && !mode.equals("any") && !arena.equals("any")) {
+            replayPage = replayRepository.findByVersionAndArenaAndGameMode(pageable, version, arena, mode);
+        }
+        return new SearchResult(replayPage.getContent());
+    }
+
+    @RequestMapping(value = "/newreplay", method = RequestMethod.POST)
+    public void addReplay(@RequestParam("replay") MultipartFile file) throws IOException {
         if (!file.getOriginalFilename().contains(".rep")) {
-            redirectAttributes.addFlashAttribute("notification", "You didn't add a proper replay file!");
-            return "redirect:/";
-        } else if (result.hasErrors()) {
-            redirectAttributes.addFlashAttribute("notification", "You left some required text fields empty!");
-            return "redirect:/";
+            return;
         }
 
         ReplayMetadataSerializer ser = new ReplayMetadataSerializer();
         ReplayHeader header = ser.readObject(ByteBuffer.wrap(file.getBytes()), ReplayHeader.class);
 
+        Replay replay = new Replay();
         replay.setContent("download_url");
-
         replay.setGameDate(header.getDate());
-
         replay.setGameMode(header.getGameMode());
-
         replay.setVersion(header.getVersion());
-
         replay.setArena(header.getArena());
-
         replay.setDownloads(0);
-
         replayRepository.save(replay);
 
         for (String playerName : header.getPlayers().values()) {
@@ -90,15 +101,12 @@ public class ReplayController {
             player.setReplay(replay);
             playerRepository.save(player);
         }
-
-        return "redirect:/";
     }
 
-    @RequestMapping(value = "/{id}/downloadtl", method = RequestMethod.POST)
-    public String downloadReplay(@PathVariable Long id) {
+    @RequestMapping(value = "/{id}/download", method = RequestMethod.POST)
+    public void downloadReplay(@PathVariable Long id) {
         Replay replay = replayRepository.findOne(id);
         replay.setDownloads(replay.getDownloads() + 1);
         replayRepository.save(replay);
-        return "redirect:/";
     }
 }
--- src/main/java/RS/repository/ReplayRepository.java
+++ src/main/java/RS/repository/ReplayRepository.java
@@ -9,4 +9,15 @@ import org.springframework.data.repository.query.Param;
 public interface ReplayRepository extends JpaRepository<Replay, Long> {
 
     Page<Replay> findAll(Pageable pageable);
+    Page<Replay> findByVersion(Pageable pageable, @Param("version") String version);
+    Page<Replay> findByArena(Pageable pageable, @Param("arena") String arena);
+    Page<Replay> findByGameMode(Pageable pageable, @Param("gameMode") String gameMode);
+    Page<Replay> findByVersionAndArena(Pageable pageable, @Param("version") String version,
+            @Param("arena") String arena);
+    Page<Replay> findByVersionAndGameMode(Pageable pageable, @Param("version") String version,
+            @Param("gameMode") String gameMode);
+    Page<Replay> findByGameModeAndArena(Pageable pageable, @Param("gameMode") String gameMode,
+            @Param("arena") String arena);
+    Page<Replay> findByVersionAndArenaAndGameMode(Pageable pageable, @Param("version") String version,
+            @Param("arena") String arena, @Param("gameMode") String gameMode);
 }
